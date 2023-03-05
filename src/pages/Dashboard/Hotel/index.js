9cfeaf552a354b4a137e1fdf4488ce1ffc583811
vsisterolli <vsisterolli@gmail.com>
2023-03-05 13:11:21 +0000

    feat: hotel boilerplate

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -1,3 +1,31 @@
+import { useContext, useEffect, useState } from 'react';
+import UserContext from '../../../contexts/UserContext';
+import { getTicket } from '../../../services/tickets';
+import getToken from '../../../hooks/useToken';
+import useToken from '../../../hooks/useToken';
+import StyledForbiddenMessage from './styles';
+
 export default function Hotel() {
-  return 'Hotel: Em breve!';
+  const [ticket, setTicket] = useState('noHotel');
+  const options = {
+    'noTicket': <StyledForbiddenMessage>Você precisa ter confirmado o <br/>pagamento antes de fazer a escolha de hospedagem</StyledForbiddenMessage>,
+    'noHotel': <StyledForbiddenMessage>Sua modalidade de ingresso não inclui hospedagem.<br/>Prossiga para a escolha de atividade</StyledForbiddenMessage>
+  };
+
+  const token = useToken();
+  useEffect(() => {
+    try {
+      const ticket = getTicket(token);
+      if(ticket.ticketType.isRemote || !ticket.ticketType.includesHotel)
+        setTicket('noHotel');
+      else
+        setTicket('hotel');
+    }
+    catch(e) {
+      setTicket('noTicket');
+    }
+  }, []);
+
+  const userData = useContext(UserContext);
+  return options[ticket];
 }
---
+++ src/pages/Dashboard/Hotel/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const StyledForbiddenMessage = styled.div`
+
+    display: flex;
+    height: 100%;
+    align-items: center;
+    justify-content: center;
+
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 23px;
+    text-align: center;
+
+    color: #8E8E8E ;
+`;
+
+export default StyledForbiddenMessage;
